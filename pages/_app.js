805d71061b3a9c418c4bf6992bba38de419f874c
grandeurspaces8 <grandeurspaces8@gmail.com>
2026-05-25 17:57:53 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,8 +5,9 @@ import Script from 'next/script'
 export default function App({ Component, pageProps }) {
   return (
     <>
+      {/* OLD GTM */}
       <Script
-        id="gtm-script"
+        id="gtm-script-old"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
@@ -21,6 +22,34 @@ export default function App({ Component, pageProps }) {
           `,
         }}
       />
+
+      {/* NEW GTM */}
+      <Script
+        id="gtm-script-new"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];
+            w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+            var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+            j.async=true;
+            j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+            f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-M4GCDQHL');
+          `,
+        }}
+      />
+
+      {/* NEW GTM NOSCRIPT */}
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-M4GCDQHL"
+          height="0"
+          width="0"
+          style={{ display: 'none', visibility: 'hidden' }}
+        />
+      </noscript>
 
       <LanguageProvider>
         <Component {...pageProps} />
